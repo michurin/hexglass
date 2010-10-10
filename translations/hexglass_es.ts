cac6f141a88f02a318b97241f7d952563db3b705
Alexey Michurin <a.michurin@gmail.com>
2010-10-10 04:12:33 +0000

    Spanish locale corrected by Ricardo A. Hermosilla Carrillo
    git-svn-id: svn://localhost/svn/hexglass/trunk@20 36197b5c-971a-4633-a35f-f2d016378fb0

--- translations/hexglass_es.ts
+++ translations/hexglass_es.ts
@@ -2,42 +2,66 @@
 <context>
     <name>Dialogs</name>
     <message>
-        <source>&lt;h3&gt;Hot keys&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Left/Right (arrows)&lt;/td&gt;&lt;dd&gt;Shift block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Up/Down (arrows)&lt;/td&gt;&lt;dd&gt;Rotate block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Space&lt;/td&gt;&lt;dd&gt;Drop block down or start new game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Force one step to down&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pause/unpause&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;New game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Change size (and restart game)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Change skin&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Qute&lt;/dd&gt;&lt;/dl&gt;</source>
-        <translation>&lt;h3&gt;Controles&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Izquierda/Derecha (cursores)&lt;/td&gt;&lt;dd&gt;Desplazar ficha&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Arriba/Abajo (cursores)&lt;/td&gt;&lt;dd&gt;Rotar ficha&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Espacio&lt;/td&gt;&lt;dd&gt;Colocar ficha o comenzar nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Intro&lt;/td&gt;&lt;dd&gt;Bajar un paso la ficha&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pausar/reanudar&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;Nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Cambiar tamaño (y reiniciar juego)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Cambiar apariencia&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Salir&lt;/dd&gt;&lt;/dl&gt;</translation>
-    </message>
-    <message>
         <source>About</source>
         <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Released under the terms of the &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;The </source>
+        <translation>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Distribuido bajo los términos de la licencia &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;</translation>
+    </message>
+    <message>
+        <source> is a Tetris-like puzzle game. 10 different types of blocks continuously fall from above and you must arrange them to make horizontal rows of hexagonal bricks. Completing any row causes those hexagonal blocks to disappear and the rest above move downwards. The blocks above gradually fall faster and the game is over when the screen fills up and blocks can no longer fall from the top.&lt;/p&gt;&lt;p&gt;Project home page: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</source>
+        <translation> es un juego de puzzle tipo Tetris. 10 distintos tipos de bloques caen continuamente desde la parte superior, por lo que deberá situarlas para formar filas horizontales de bloques hexagonales. Completando una fila de esos bloques hará que éstos desaparezcan y posteriormente los bloques superiores desciendan. Los bloques caerán cada vez más rápido y el juego terminará cuando la pantalla se llene de bloques y no puedan caer más desde arriba.&lt;/p&gt;&lt;p&gt;Página principal del Proyecto: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Help</source>
         <translation>Ayuda</translation>
     </message>
     <message>
-        <source> is a Tetris-like puzzle game. 10 different types of blocks continuously fall from above and you must arrange them to make horizontal rows of hexagonal bricks. Completing any row causes those hexagonal blocks to disappear and the rest above move downwards. The blocks above gradually fall faster and the game is over when the screen fills up and blocks can no longer fall from the top.&lt;/p&gt;&lt;p&gt;Project home page: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</source>
-        <translation> es un juego tipo Tedris. 10 tipos distintos de fichas descenderán contínuamente y debes situarlas para formar filas horizontales de bloques hexagonales. Completar una fila hará que desaparezcan los bloques hexagonales. Los bloques irán cayendo cada vez más rápido y el juego terminará cuando la pantalla se llene de bloques y no puedan caer más desde arriba.&lt;/p&gt;&lt;p&gt;Página principal del proyecto: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</translation>
-    </message>
-    <message>
-        <source>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Released under the terms of the &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;The </source>
-        <translation>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Distribuído bajo los términos de la licencia &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;</translation>
+        <source>&lt;h3&gt;Hot keys&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Left/Right (arrows)&lt;/td&gt;&lt;dd&gt;Shift block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Up/Down (arrows)&lt;/td&gt;&lt;dd&gt;Rotate block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Space&lt;/td&gt;&lt;dd&gt;Drop block down or start new game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Force one step to down&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pause/unpause&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;New game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Change size (and restart game)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Change skin&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Qute&lt;/dd&gt;&lt;/dl&gt;</source>
+        <translation>&lt;h3&gt;Controles&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Izquierda/Derecha (flechas)&lt;/td&gt;&lt;dd&gt;Desplazar bloque&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Arriba/Abajo (flechas)&lt;/td&gt;&lt;dd&gt;Rotar bloque&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Espacio&lt;/td&gt;&lt;dd&gt;Descender bloque rápidamente o empezar nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Forzar un paso hacia abajo&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pausar/Reanudar&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;Nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Cambiar el tamaño (y reiniciar el juego)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Cambiar Apariencia&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Salir&lt;/dd&gt;&lt;/dl&gt;</translation>
     </message>
 </context>
 <context>
     <name>GlassWidget</name>
     <message>
         <source>pause</source>
-        <translation>pausar</translation>
+        <translation>pausa</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
+    <message>
+        <source>Next</source>
+        <translation>Siguiente</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Puntuación</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Líneas</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Nivel</translation>
+    </message>
     <message>
         <source>Game</source>
         <translation>Juego</translation>
     </message>
     <message>
         <source>New game</source>
-        <translation>Nuevo juego</translation>
+        <translation>Nuevo Juego</translation>
+    </message>
+    <message>
+        <source>Autopause mode</source>
+        <translation>Modo auto-pausa</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Tamaño</translation>
     </message>
     <message>
         <source>Tiny</source>
@@ -58,6 +82,10 @@
     <message>
         <source>Huge</source>
         <translation>Enorme</translation>
+    </message>
+    <message>
+        <source>Skin</source>
+        <translation>Apariencia</translation>
     </message>
     <message>
         <source>Mosaic normal</source>
@@ -90,34 +118,6 @@
     <message>
         <source>Quit</source>
         <translation>Salir</translation>
-    </message>
-    <message>
-        <source>Next</source>
-        <translation>Siguiente</translation>
-    </message>
-    <message>
-        <source>Score</source>
-        <translation>Puntuación</translation>
-    </message>
-    <message>
-        <source>Lines</source>
-        <translation>Líneas</translation>
-    </message>
-    <message>
-        <source>Level</source>
-        <translation>Nivel</translation>
-    </message>
-    <message>
-        <source>Size</source>
-        <translation>Tamaño</translation>
-    </message>
-    <message>
-        <source>Skin</source>
-        <translation>Apariencia</translation>
-    </message>
-    <message>
-        <source>Autopause mode</source>
-        <translation>Modo autopausa</translation>
     </message>
 </context>
 </TS>
