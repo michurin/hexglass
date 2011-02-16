ceb85409d1e399e69de65b2d143e3d8dd62c6cad
Alexey Michurin <a.michurin@gmail.com>
2011-02-16 16:01:55 +0000

    French locale. Thanks a lot to Vincent Petry.
    git-svn-id: svn://localhost/svn/hexglass/trunk@74 36197b5c-971a-4633-a35f-f2d016378fb0

--- hexglass.pro
+++ hexglass.pro
@@ -107,6 +107,7 @@ TRANSLATIONS = translations/hexglass_be.ts \
                translations/hexglass_cs.ts \
                translations/hexglass_el.ts \
                translations/hexglass_es.ts \
+               translations/hexglass_fr.ts \
                translations/hexglass_ru.ts \
                translations/hexglass_uk.ts \
                translations/hexglass_zh.ts
---
+++ translations/hexglass_fr.ts
@@ -0,0 +1,139 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Dialogs</name>
+    <message>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Copyright (c) 2010-2011 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Released under the terms of the &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;The </source>
+        <translation>&lt;p&gt;Copyright (c) 2010-2011 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Distribué sous les termes de la license &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;</translation>
+    </message>
+    <message>
+        <source> is a Tetris-like puzzle game. 10 different types of blocks continuously fall from above and you must arrange them to make horizontal rows of hexagonal bricks. Completing any row causes those hexagonal blocks to disappear and the rest above move downwards. The blocks above gradually fall faster and the game is over when the screen fills up and blocks can no longer fall from the top.&lt;/p&gt;&lt;p&gt;Project home page: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</source>
+        <translation> est un jeu de puzzle dans le style de Tetris. 10 types de blocs différents tombent continuellement de la partie supérieure de l&apos;écran, vous devez les arranger pour créer des lignes horizontales de briques hexagonales. Le fait de compléter une ligne fera disparaître ces blocs hexagonaux, et fera tomber les blocs se trouvant sur ceux-ci. La vitesse de chute des blocs augmente graduellement et le jeu se termine lorsque le tableau est rempli et qu&apos;aucun bloc ne peut être ajouté.&lt;/p&gt;&lt;p&gt;Site Internet du projet : &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Hot keys&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Left/Right (arrows)&lt;/td&gt;&lt;dd&gt;Shift block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Up/Down (arrows)&lt;/td&gt;&lt;dd&gt;Rotate block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Space&lt;/td&gt;&lt;dd&gt;Drop block down or start new game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Force one step to down&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pause/unpause&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;New game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Change size (and restart game)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Change skin&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Quit&lt;/dd&gt;&lt;/dl&gt;</source>
+        <translation>&lt;h3&gt;Contrôles&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Gauche/Droite (flèches)&lt;/td&gt;&lt;dd&gt;Déplacer bloc&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Haut/Bas  (flèches)&lt;/td&gt;&lt;dd&gt;Rotation bloc&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Espace&lt;/td&gt;&lt;dd&gt;Déscendre rapidement le bloc ou commencer une nouvelle partie&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Entrée&lt;/td&gt;&lt;dd&gt;Accélérer temporairement la déscente du bloc&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pause/Reprendre&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;Nouvelle partie&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Changer la taille (et recommencer la partie)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Changer l&apos;apparence&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Quitter&lt;/dd&gt;&lt;/dl&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>GlassWidget</name>
+    <message>
+        <source>pause</source>
+        <translation>pause</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Score</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Lignes</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Niveau</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation>Partie</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Nouvelle partie</translation>
+    </message>
+    <message>
+        <source>Autopause mode</source>
+        <translation>Mode pause automatique</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Taille</translation>
+    </message>
+    <message>
+        <source>Tiny</source>
+        <translation>Minuscule</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Petit</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <source>Big</source>
+        <translation>Grand</translation>
+    </message>
+    <message>
+        <source>Huge</source>
+        <translation>Énorme</translation>
+    </message>
+    <message>
+        <source>Skin</source>
+        <translation>Apparence</translation>
+    </message>
+    <message>
+        <source>Mosaic normal</source>
+        <translation>Mosaïque normale</translation>
+    </message>
+    <message>
+        <source>Mosaic big</source>
+        <translation>Mosaïque grande</translation>
+    </message>
+    <message>
+        <source>Aqua normal</source>
+        <translation>Agua normal</translation>
+    </message>
+    <message>
+        <source>Aqua big</source>
+        <translation>Aqua grand</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>A propos de Qt</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>Careful dropping</source>
+        <translation>Positionner avec précision</translation>
+    </message>
+    <message>
+        <source>High scores</source>
+        <translation>Meilleurs scores</translation>
+    </message>
+    <message>
+        <source>Mosaic tiny</source>
+        <translation>Mosaïque petite</translation>
+    </message>
+    <message>
+        <source>Aqua huge</source>
+        <translation>Agua énorme</translation>
+    </message>
+</context>
+</TS>
